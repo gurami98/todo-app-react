1f495be72a0699f979114d46b1d8fd8c3175fbfa
Guram Chilindrishvili <g.chilindrishvili.exceedteam@gmail.com>
2021-07-06 13:32:12 +0400

    features - add task type, due date, time added, sort feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import styled from "styled-components";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import List from './components/List'
 import Form from './components/Form'
 import Pagination from './components/Pagination'
@@ -32,8 +32,19 @@ const App = () => {
 	const [list, setList] = useState([])
 	const [activePage, setActive] = useState(1)
 	const [itemsToShow, setItemsToShow] = useState(8)
+	const [dueDate, setDueDate] = useState('')
+	const filterDropdownItemsRef = useRef(null)
+	const filterDropdownBtn = useRef(null)
+	//const [filterText, setFilterText] = useState('')
 
 	const [paginationInfo, setPaginationInfo] = useState({pageNumbers: 1, pagesToShow: 5, endPage: 1, startPage: 1})
+	const [typeDropdown, setTypeDropdown] = useState(		{typeDropdownShow: false,
+																																typeDropdownData: ['University', 'Home', 'Work'],
+																																typeDropdownInput: '',
+																																typeDropdownText: 'Task Type'})
+	const [filterDropdown, setFilterDropdown] = useState({filterDropdownShow: false,
+																																	filterDropdownData: ['A-Z', 'Z-A', 'Oldest First', 'Newest First', 'Due Ascending', 'Due Descending'],
+																																	filterDropdownText: 'Sort By'})
 
 	const [checkedAll, setCheckedAll] = useState(false)
 
@@ -44,6 +55,44 @@ const App = () => {
 	let endIndex = startIndex + itemsToShow
 	let itemsArr = list.slice(startIndex, endIndex)
 
+	const showFilterDropDown = (e) => {
+		e.preventDefault();
+		console.log('test')
+		setFilterDropdown({...filterDropdown, filterDropdownShow: !filterDropdown.filterDropdownShow})
+	}
+
+	const choseFilterType = (e) => {
+		let tempArr = [...list]
+		switch(e.target.innerHTML) {
+			case 'A-Z':
+				tempArr = tempArr.sort((a, b) => a.text.localeCompare(b.text))
+				setList(tempArr)
+				break;
+			case 'Z-A':
+				tempArr = tempArr.sort((a, b) => b.text.localeCompare(a.text))
+				setList(tempArr)
+				break;
+			case 'Newest First':
+				tempArr = tempArr.sort((a,b) => new Date(b.timeAdded) - new Date(a.timeAdded))
+				setList(tempArr)
+				break;
+			case 'Oldest First':
+				tempArr = tempArr.sort((a,b) => new Date(a.timeAdded) - new Date(b.timeAdded))
+				setList(tempArr)
+				break;
+			case 'Due Ascending':
+				tempArr = tempArr.sort((a,b) => new Date(a.dueDate) - new Date(b.dueDate))
+				setList(tempArr)
+				break;
+			case 'Due Descending':
+				tempArr = tempArr.sort((a,b) => new Date(b.dueDate) - new Date(a.dueDate))
+				setList(tempArr)
+				break;
+			default:
+				break;
+		}
+	}
+
 	useEffect(() => {
 		let counter = 0;
 		list.forEach(item => {
@@ -53,10 +102,39 @@ const App = () => {
 		if(counter === list.length && counter > 0) setCheckedAll(true)
 	}, [list])
 
+	useEffect(() => {
+		const handleClickOutside = (e) => {
+			if (filterDropdownItemsRef.current && !filterDropdownBtn.current.contains(e.target) && !filterDropdownItemsRef.current.contains(e.target) && filterDropdownItemsRef.current.classList.contains('show')) {
+				setFilterDropdown({...filterDropdown, filterDropdownShow: false})
+			}
+		}
+		// Bind the event listener
+		document.addEventListener("mousedown", handleClickOutside);
+		return () => {
+			// Unbind the event listener on clean up
+			document.removeEventListener("mousedown", handleClickOutside);
+		};
+	}, [filterDropdownItemsRef]);
+
 	const handleSubmit = (text) => {
-		let newArr = [...list, {text, done: false, id: new Date().getTime()}]
-		if (text.trim()) {
+		if (text.trim() && typeDropdown.typeDropdownText !== 'Task Type' && dueDate.trim()) {
+			let d = new Date(),
+				seconds = '' + d.getSeconds(),
+				minutes = '' + d.getMinutes(),
+				hours = '' + d.getHours(),
+				month = '' + (d.getMonth() + 1),
+				day = '' + d.getDate(),
+				year = d.getFullYear();
+			if(seconds < 10) seconds = '0' + seconds
+			if(minutes < 10) minutes = '0' + minutes
+			if (month.length < 2) month = '0' + month;
+			if (day.length < 2) day = '0' + day;
+			let dateAdded = [year, month, day].join(' ').concat(" ", [hours, minutes, seconds].join(':'));
+
+			let newArr = [...list, {text, taskType: typeDropdown.typeDropdownText, dueDate, timeAdded: dateAdded, done: false, id: new Date().getTime()}]
+
 			setList(newArr)
+			setTypeDropdown({...typeDropdown, typeDropdownText: 'Task Type'})
 			let listCount = newArr.length
 			let pageCount = Math.ceil(listCount / itemsToShow)
 			if (!listCount) setPaginationInfo({...paginationInfo, pageNumbers: [1]})
@@ -75,7 +153,7 @@ const App = () => {
 			}
 			setActive(pageCount)
 			setPaginationInfo({...paginationInfo, endPage: pageCount, startPage: pageCount > 5 ? pageCount - paginationInfo.pagesToShow + 1 : 1})
-		} else alert('Enter an item')
+		} else alert('Enter every value needed in form')
 	}
 
 	const changePage = (page) => {
@@ -121,9 +199,17 @@ const App = () => {
 			if(item.done) isAnyItemChecked = true
 	})
 
+	// const handleFilterKeyPress = (e) => {
+	// 	if (e.key === 'Enter') filterItems()
+	// }
+	//
+	// const filterItems = () => {
+	// 	console.log(filterText)
+	// }
+
 	return (
 		<div className="App">
-			<Form handleSubmit={handleSubmit}/>
+			<Form handleSubmit={handleSubmit} dueDate={dueDate} setDueDate={setDueDate} typeDropdown={typeDropdown} setTypeDropdown={setTypeDropdown}/>
 			<Dropdown  paginationInfo={paginationInfo} setPaginationInfo={setPaginationInfo}
 								listCount={listCount} pageCount={pageCount}
 			           setActive={setActive} itemsToShow={itemsToShow}
@@ -133,6 +219,21 @@ const App = () => {
 				<input disabled={!listCount} type="checkbox" id="select-all" name="select-all" checked={checkedAll} onChange={() => tick()}/>
 				<label htmlFor="select-all">Select All</label>
 				<Button disabled={!isAnyItemChecked} onClick={()=>deleteSelected()}>Delete Selected</Button>
+				{/*<label htmlFor="filter-items" id="filter-label">Filter</label>*/}
+				{/*<input value={filterText} onChange={(e)=>setFilterText(e.target.value)} onKeyDown={handleFilterKeyPress} type="text" id="filter-items" name="filter-items"/>*/}
+				{/*<button type="button" onClick={filterItems}>Filter</button>*/}
+				<div className="dropdown">
+					<button ref={filterDropdownBtn} onClick={(e)=> showFilterDropDown(e)} className="dropbtn" type="button">{filterDropdown.filterDropdownText}</button>
+					<div id="myDropdown" ref={filterDropdownItemsRef} className={filterDropdown.filterDropdownShow ? "dropdown-content show" : "dropdown-content"}>
+						<div className={"dropdown-items"}>
+							{filterDropdown.filterDropdownData.map(item => {
+								return(
+									<p onClick={(e)=>choseFilterType(e)} key={item}>{item}</p>
+								)
+							})}
+						</div>
+					</div>
+				</div>
 			</div>
 
 			<List paginationInfo={paginationInfo} setPaginationInfo={setPaginationInfo}
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,15 @@
 import styled from "styled-components";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const StyledForm = styled.form`
 	width: 500px;
   display: flex;
   flex-direction: row;
+	flex-wrap: wrap;
   margin: 10px;
   padding: 10px;
   border: 1px solid gray;
-  border-radius: 3px;
+  border-radius: 3px;	
 	
 	@media (max-width: 800px){
 		flex-direction: column;
@@ -24,8 +25,7 @@ const StyledForm = styled.form`
 const Input = styled.input`
   text-indent: 5px;
   border-radius: 5px;
-	width: 400px;
-	margin-right: 25px;
+	width: 300px;
   height: 25px;
   outline: none;
   border: 1px solid gray;
@@ -48,17 +48,88 @@ const Button = styled.button`
   background-color: #329ea8;
 `
 
-const Form = ({handleSubmit}) => {
+const Form = ({handleSubmit, typeDropdown, setTypeDropdown, dueDate, setDueDate}) => {
 	const [text, setText] = useState('')
+	const dropdownItemsRef = useRef(null)
+	const dropdownBtn = useRef(null)
+
+	useEffect(() => {
+		let btnText = typeDropdown.typeDropdownText
+		const handleClickOutside = (e) => {
+			if (dropdownItemsRef.current && !dropdownBtn.current.contains(e.target) && !dropdownItemsRef.current.contains(e.target) && dropdownItemsRef.current.classList.contains('show')) {
+				// small bug, button text resets after clicking outside of button
+				setTypeDropdown({...typeDropdown, typeDropdownText: btnText, typeDropdownShow: false})
+			}
+		}
+		// Bind the event listener
+		document.addEventListener("mousedown", handleClickOutside);
+		return () => {
+			// Unbind the event listener on clean up
+			document.removeEventListener("mousedown", handleClickOutside);
+		};
+	}, [dropdownItemsRef]);
+
+	const handleFormInputKeyPress = (e) => {
+		if (e.key === 'Enter') handleFormInput(e)
+	}
+	const handleFormInput = (e) => {
+		handleSubmit(text)
+		setText('')
+		e.preventDefault()
+	}
+
+	const showDropDown = (e) => {
+		e.preventDefault();
+		setTypeDropdown({...typeDropdown, typeDropdownShow: !typeDropdown.typeDropdownShow})
+	}
+
+	const handleInputChange = (e) => {
+		setTypeDropdown({...typeDropdown, typeDropdownInput: e.target.value})
+	}
+
+	const handleDropInputKeyPress = (e) => {
+		if (e.key === 'Enter') handleAddType(e)
+	}
+
+	const handleAddType = (e) => {
+		e.preventDefault();
+		if (typeDropdown.typeDropdownInput.trim())
+			setTypeDropdown({...typeDropdown, typeDropdownInput: '', typeDropdownData: [...typeDropdown.typeDropdownData, typeDropdown.typeDropdownInput]})
+	}
+
+	const choseType = (e) => {
+		console.log(e.target.innerHTML)
+		setTypeDropdown({...typeDropdown, typeDropdownText: e.target.innerHTML, typeDropdownShow: !typeDropdown.typeDropdownShow})
+	}
+
+	const handleDate = (e) => {
+		setDueDate(e.target.value)
+	}
 
 	return (
-		<StyledForm onSubmit={(e) => {
-			handleSubmit(text)
-			setText('')
-			e.preventDefault()
-		}}>
-			<Input autoFocus type="text" value={text} onChange={e => setText(e.target.value)} placeholder='New Todo'/>
-			<Button type='submit'>Add Task</Button>
+		<StyledForm>
+			<Input autoFocus type="text" value={text} onChange={e => setText(e.target.value)} onKeyDown={handleFormInputKeyPress} placeholder='New Todo'/>
+			<div className="dropdown">
+				<button ref={dropdownBtn} onClick={(e)=> showDropDown(e)} className="dropbtn" type="button">{typeDropdown.typeDropdownText}</button>
+				<div id="myDropdown" ref={dropdownItemsRef} className={typeDropdown.typeDropdownShow ? "dropdown-content show" : "dropdown-content"}>
+					<div className={"dropdown-items"}>
+						{typeDropdown.typeDropdownData.map(item => {
+							return(
+								<p onClick={choseType} key={item}>{item}</p>
+							)
+						})}
+					</div>
+					<div className={"add-new-item"}>
+						<input type="text" value={typeDropdown.typeDropdownInput} onKeyDown={handleDropInputKeyPress} onChange={(e)=> handleInputChange(e)}/>
+						<button type="button" autoFocus  onClick={handleAddType}>Add</button>
+					</div>
+				</div>
+			</div>
+			<Button type='button' onClick={handleFormInput}>
+				Add Task
+			</Button>
+			<label htmlFor="due-date">Due Date:</label>
+			<input value={dueDate} onChange={(e) => handleDate(e)} type="date" id="due-date" name="due-date"/>
 		</StyledForm>
 	)
 }
